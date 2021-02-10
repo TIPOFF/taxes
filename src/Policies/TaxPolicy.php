2dcc512607379245af636d63d270ddb88086da82
Drew Roberts <github@drewroberts.com>
2021-02-10 02:04:12 -0500

    Update user model reference

--- src/Policies/TaxPolicy.php
+++ src/Policies/TaxPolicy.php
@@ -2,21 +2,22 @@
 
 namespace Tipoff\Taxes\Policies;
 
-use app('user') as User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Tipoff\Taxes\Models\Tax;
 
 class TaxPolicy
 {
     use HandlesAuthorization;
+    
+    /** @var Model $roomModel */
+    $user = app('user');
 
     /**
      * Determine whether the user can view any models.
      *
-     * @param  app('user') $user
      * @return mixed
      */
-    public function viewAny(User $user)
+    public function viewAny($user)
     {
         return $user->hasPermissionTo('view taxes') ? true : false;
     }
@@ -27,7 +28,7 @@ class TaxPolicy
      * @param Tax $tax
      * @return mixed
      */
-    public function view(User $user, Tax $tax)
+    public function view($user, Tax $tax)
     {
         return $user->hasPermissionTo('view taxes') ? true : false;
     }
@@ -35,10 +36,9 @@ class TaxPolicy
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
      * @return mixed
      */
-    public function create(User $user)
+    public function create($user)
     {
         return $user->hasPermissionTo('create taxes') ? true : false;
     }
@@ -46,11 +46,10 @@ class TaxPolicy
     /**
      * Determine whether the user can update the model.
      *
-     * @param \App\Models\User $user
      * @param Tax $tax
      * @return mixed
      */
-    public function update(User $user, Tax $tax)
+    public function update($user, Tax $tax)
     {
         return $user->hasPermissionTo('update taxes') ? true : false;
     }
@@ -58,11 +57,10 @@ class TaxPolicy
     /**
      * Determine whether the user can delete the model.
      *
-     * @param \App\Models\User $user
      * @param Tax $tax
      * @return mixed
      */
-    public function delete(User $user, Tax $tax)
+    public function delete($user, Tax $tax)
     {
         return false;
     }
@@ -70,11 +68,10 @@ class TaxPolicy
     /**
      * Determine whether the user can restore the model.
      *
-     * @param \App\Models\User $user
      * @param Tax $tax
      * @return mixed
      */
-    public function restore(User $user, Tax $tax)
+    public function restore($user, Tax $tax)
     {
         return false;
     }
@@ -82,11 +79,10 @@ class TaxPolicy
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param \App\Models\User $user
      * @param Tax $tax
      * @return mixed
      */
-    public function forceDelete(User $user, Tax $tax)
+    public function forceDelete($user, Tax $tax)
     {
         return false;
     }
