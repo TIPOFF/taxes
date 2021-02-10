d7d664606e97fd9682f03fe8b661a7c20fc8b495
Chris H <crishack8@gmail.com>
2021-02-09 23:09:57 -0600

    Add Policies

---
+++ src/Policies/TaxPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tipoff\Taxes\Policies;
+
+use Tipoff\Taxes\Models\Tax;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaxPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return $user->hasPermissionTo('view taxes') ? true : false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Tax  $tax
+     * @return mixed
+     */
+    public function view(User $user, Tax $tax)
+    {
+        return $user->hasPermissionTo('view taxes') ? true : false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->hasPermissionTo('create taxes') ? true : false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Tax  $tax
+     * @return mixed
+     */
+    public function update(User $user, Tax $tax)
+    {
+        return $user->hasPermissionTo('update taxes') ? true : false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Tax  $tax
+     * @return mixed
+     */
+    public function delete(User $user, Tax $tax)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Tax  $tax
+     * @return mixed
+     */
+    public function restore(User $user, Tax $tax)
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Tax  $tax
+     * @return mixed
+     */
+    public function forceDelete(User $user, Tax $tax)
+    {
+        return false;
+    }
+}
