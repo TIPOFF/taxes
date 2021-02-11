b5754300136af14e7f6eb6a36bf18a61e3d7f56c
Patrick Breen <patrick.breen@quartzy.com>
2021-02-11 09:08:18 -0500

    Include SupportServiceProvider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tipoff\Taxes\Tests;
 
+use Tipoff\Support\SupportServiceProvider;
 use Tipoff\Taxes\TaxesServiceProvider;
 use Tipoff\TestSupport\BaseTestCase;
 
@@ -12,6 +13,7 @@ class TestCase extends BaseTestCase
     protected function getPackageProviders($app)
     {
         return [
+            SupportServiceProvider::class,
             TaxesServiceProvider::class,
         ];
     }
