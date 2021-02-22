a6daa20519a1b6311a2b9edf3f4a0dc49ee70d8e
Drew Roberts <github@drewroberts.com>
2021-02-22 09:00:22 -0500

    Consolidate tax permissions

--- database/migrations/2021_02_12_021016_add_tax_permissions.php
+++ database/migrations/2021_02_12_021016_add_tax_permissions.php
@@ -17,7 +17,11 @@ class AddTaxPermissions extends Migration
                          'view taxes',
                          'create taxes',
                          'update taxes',
-                         'delete taxes'
+                         'delete taxes',
+                         'view location taxes',
+                         'create location taxes',
+                         'update location taxes',
+                         'delete location taxes',
                      ] as $name) {
                 app(Permission::class)::findOrCreate($name, null);
             };
