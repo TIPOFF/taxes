220a16f4d6db1b05377a2648900bb07fcf8c5231
Drew Roberts <github@drewroberts.com>
2021-02-22 09:03:11 -0500

    Remove delete permissions

--- database/migrations/2021_02_12_021016_add_tax_permissions.php
+++ database/migrations/2021_02_12_021016_add_tax_permissions.php
@@ -17,11 +17,9 @@ class AddTaxPermissions extends Migration
                          'view taxes',
                          'create taxes',
                          'update taxes',
-                         'delete taxes',
                          'view location taxes',
                          'create location taxes',
                          'update location taxes',
-                         'delete location taxes',
                      ] as $name) {
                 app(Permission::class)::findOrCreate($name, null);
             };
