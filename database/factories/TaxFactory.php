a4930f3a38f9308b0d79ec1f33553d17263dd3de
Drew Roberts <github@drewroberts.com>
2021-03-20 14:30:52 -0400

    Update factory for tax #44

--- database/factories/TaxFactory.php
+++ database/factories/TaxFactory.php
@@ -6,35 +6,26 @@ namespace Tipoff\Taxes\Database\Factories;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Tipoff\Locations\Models\Location;
 use Tipoff\Taxes\Enum\TaxCode;
 use Tipoff\Taxes\Models\Tax;
 
 class TaxFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Tax::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         $sentence = $this->faker->sentence;
 
         return [
-            'slug'       => Str::slug($sentence),
-            'name'       => $sentence,
-            'title'      => $sentence,
-            'percent'    => $this->faker->numberBetween(1, 50),
-            'tax_code'   => $this->faker->randomElement(TaxCode::getValues()),
-            'location_id'=> randomOrCreate(app('location')),
-            'creator_id' => randomOrCreate(app('user')),
+            'slug'          => Str::slug($sentence),
+            'name'          => $sentence,
+            'title'         => $sentence,
+            'percent'       => $this->faker->numberBetween(1, 50),
+            'tax_code'      => $this->faker->randomElement(TaxCode::getValues()),
+            'location_id'   => Location::factory()->create(),
+            'creator_id'    => randomOrCreate(app('user')),
         ];
     }
 }
