01a31b56941f232e753d7db486ff6588584e5bc0
Drew Roberts <github@drewroberts.com>
2021-03-28 14:06:58 -0400

    Update tax resource test #2

--- tests/Feature/TaxResourceTest.php
+++ tests/Feature/TaxResourceTest.php
@@ -5,22 +5,48 @@ declare(strict_types=1);
 namespace Tipoff\Taxes\Tests\Feature\Nova;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tipoff\Authorization\Models\User;
 use Tipoff\Taxes\Models\Tax;
 use Tipoff\Taxes\Tests\TestCase;
 
-class TaxResourceTest extends TestCase
+class ProductResourceTest extends TestCase
 {
     use DatabaseTransactions;
-
-    /** @test */
-    public function index()
+    
+    private const NOVA_ROUTE = 'nova-api/taxes';
+    
+    /**
+     * @dataProvider dataProviderForIndexByRole
+     * @test
+     */
+    public function index_by_role(?string $role, bool $hasAccess, bool $canIndex)
     {
         Tax::factory()->count(4)->create();
 
-        $this->actingAs(self::createPermissionedUser('view taxes', true));
+        $user = User::factory()->create();
+        if ($role) {
+            $user->assignRole($role);
+        }
+        $this->actingAs($user);
 
-        $response = $this->getJson('nova-api/taxes')->assertOk();
+        $response = $this->getJson(self::NOVA_ROUTE)
+            ->assertStatus($hasAccess ? 200 : 403);
 
-        $this->assertCount(4, $response->json('resources'));
+        if ($hasAccess) {
+            $this->assertCount($canIndex ? 4 : 0, $response->json('resources'));
+        }
+    }
+
+    public function dataProviderForIndexByRole()
+    {
+        return [
+            'Admin' => ['Admin', true, true],
+            'Owner' => ['Owner', true, true],
+            'Executive' => ['Executive', true, true],
+            'Staff' => ['Staff', true, true],
+            'Former Staff' => ['Former Staff', false, false],
+            'Customer' => ['Customer', false, false],
+            'No Role' => [null, false, false],
+        ];
     }
 }
